4674f9a82d8095759bd4d5c7ec230b19d1ea26a8
Satrio Teleng Bagaskoro <telengbagaskorosatrio@gmail.com>
2019-02-27 20:18:53 +0700

    - Setup project - Get random dice

--- Dice/ViewController.swift
+++ Dice/ViewController.swift
@@ -9,12 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var randomDice1: Int = 0
+    var randomDice2: Int = 0
+    
+    @IBOutlet weak var dice1: UIImageView!
+    @IBOutlet weak var dice2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    
+    @IBAction func rollPressed(_ sender: UIButton) {
+        randomDice1 = getRandom()
+        randomDice2 = getRandom()
+        
+    }
+    
+    func getRandom() -> Int {
+        return Int.random(in: 1 ... 6)
+    }
 }
 
--- DiceTests/DiceTests.swift
+++ DiceTests/DiceTests.swift
@@ -10,7 +10,8 @@ import XCTest
 @testable import Dice
 
 class DiceTests: XCTestCase {
-
+    let main = ViewController()
+    
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -19,7 +20,35 @@ class DiceTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
+    func testRandomInteger() {
+        let random = main.getRandom()
+        var expected = false
+        
+        switch random {
+        case 1:
+            expected = true
+            break
+        case 2:
+            expected = true
+            break
+        case 3:
+            expected = true
+            break
+        case 4:
+            expected = true
+            break
+        case 5:
+            expected = true
+            break
+        case 6:
+            expected = true
+            break
+        default:
+            expected = false
+        }
+        
+        XCTAssert(expected == true)
+        
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
